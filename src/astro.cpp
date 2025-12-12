2537d6f484760ac0327b89b0aa735f2ca529ef23
Björn Bethge <bjoern.bethge@gmail.com>
2025-12-12 15:31:11 +0100

    Fix: update both header files and remove deprecated ExtensionUtil

--- src/astro.cpp
+++ src/astro.cpp
@@ -6,7 +6,6 @@
 #include "duckdb/common/string_util.hpp"
 #include "duckdb/function/scalar_function.hpp"
 #include "duckdb/function/table_function.hpp"
-#include "duckdb/main/extension_util.hpp"
 #include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
 #include "duckdb/common/vector_operations/ternary_executor.hpp"
 #include "duckdb/common/arrow/arrow_util.hpp"
